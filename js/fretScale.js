137e153f986517a7ffc726bbba09c543bb5e407d
hjlemon <152624760+hjlemon@users.noreply.github.com>
2024-01-14 23:12:56 -0500

    Update fretScale.js

--- js/fretScale.js
+++ js/fretScale.js
@@ -1,31 +1,54 @@
-// Calculates the the scale length
-
-function fretScaleCalc(){
-
-let scaleLength = prompt("What is your desired scale length?"); // Prompts user for the scale length of their instrument
-let numFrets = prompt("How many frets will you have?"); //Prompts user for the number of frets on their instrument
-let testing = 0; // Placeholder value, updated by the below for loop
-let distFromNut = 0; // Placeholder value, updated by the below for loop
-const fretDists = []; // An array that is filled with values by the below for loop
-let main_table = document.getElementById("scaleLengthTable");
-
-for (let i = 0; i < numFrets; i++) {
-	// Create new row and cells for the table
-	let row = document.createElement("tr");
-	let cell1 = document.createElement("td");
-	let cell2 = document.createElement("td");
-	
-	testing = (scaleLength - distFromNut) / 17.817;
-	fretDists.push(testing);
-	distFromNut = fretDists[i] + distFromNut;
-
-	// Add the cells to the row and the row to the table body
-	cell1.appendChild(document.createTextNode(i + 1));
-	cell2.appendChild(document.createTextNode(distFromNut.toFixed(4) + '"'));
-	row.appendChild(cell1);
-	row.appendChild(cell2);
-	main_table.appendChild(row);
-			
-	document.getElementById("tableHeader").innerHTML = "Scale Length = " + scaleLength + '" -  ' + numFrets + " Frets";
+function promptForScaleLength() { // Prompts user for the scale length of their instrument
+	scaleLength = prompt("What is your desired scale length?"); 
+	document.getElementById("printScaleLength").innerHTML = "Your scale length is " + scaleLength + " inches.";
 }
+const fretDists = []; // An array that is filled with values by the below for loop	
+function promptForNumFrets() { // Prompts user for the number of frets on their instrument
+	numFrets = prompt("How many frets will you have?"); 
+	document.getElementById("printNumFrets").innerHTML = "You have " + numFrets + " frets.";
+
+	
+	let testing = 0; // Placeholder value, updated by the below for loop
+	let distFromNut = 0; // Placeholder value, updated by the below for loop
+	let main_table = document.getElementById("scaleLengthTable");
+
+	for (let i = 0; i < numFrets; i++) {
+		// Create new row and cells for the table
+		let row = document.createElement("tr");
+		let cell1 = document.createElement("td");
+		let cell2 = document.createElement("td");
+	
+		testing = (scaleLength - distFromNut) / 17.817;
+		fretDists.push(testing);
+		distFromNut = fretDists[i] + distFromNut;
+
+		// Add the cells to the row and the row to the table body
+		cell1.appendChild(document.createTextNode(i + 1));
+		cell2.appendChild(document.createTextNode(distFromNut.toFixed(4) + '"'));
+		row.appendChild(cell1);
+		row.appendChild(cell2);
+		main_table.appendChild(row);
+			
+		document.getElementById("tableHeader").innerHTML = "Scale Length = " + scaleLength + '" -  ' + numFrets + " Frets";
+	}
+}
+
+function oneToTwelveDist() {
+	let distBetween = 0;
+	for (let j = 1; j < 12; j++) {
+		distBetween = distBetween + fretDists[j];
+	}
+	
+	document.getElementById("1to12Dist").innerHTML = "The distance from the 1st fret to the 12th fret is " + distBetween.toFixed(4) + " inches.";
+}
+
+function crownHeight() {
+	let crownHeight = prompt("What is the crown height of your frets?");
+	document.getElementById("crownHeight").innerHTML = "The crown height of your frets is " + crownHeight + " inches.";
+}
+
+function stringHeight() {
+	let firstHeight = prompt("What is your desired string height above the 1st fret?");
+	let twelveHeight = prompt("What is your desired string height above the 12th fret?");
+	document.getElementById("stringHeight").innerHTML = "The desired height of your string is " + firstHeight + " inches above the 1st fret, and " + twelveHeight + " inches above the 12th fret.";
 }
